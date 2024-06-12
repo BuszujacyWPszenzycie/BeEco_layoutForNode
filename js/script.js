83d4afd49ec73005af41cf44f47da6e0e9954695
Marcin <marcin.szturminski@gmail.com>
2024-06-12 21:02:53 +0200

    added modal for search results and spinner

--- js/script.js
+++ js/script.js
@@ -1,17 +1,37 @@
-const loginModal = document.querySelector('dialog')
+const allModals = document.querySelectorAll('dialog')
 const allNavItems = document.querySelectorAll('.nav__itemWrapper')
 const closeModalIcon = document.querySelector('.login__icon')
+const searchButton = document.querySelector('.search__button')
+const searchInput = document.querySelector('.search__input')
+const browserButton = document.querySelector('.browser__button')
+const browserResults = document.querySelector('.browser__results')
+const spinner = document.querySelector('.spinner')
+console.log(spinner)
 
-console.log('test')
+const hideSpinnerFunction = () => {
+	setTimeout(function () {
+		spinner.classList.add('spinner-deactive')
+	}, 3000)
+}
 
 const showLoginPageFunction = () => {
-	console.log('is working?')
-	loginModal.showModal()
+	allModals[0].showModal()
 }
 
 const closeLoginPageFunction = () => {
-	loginModal.close()
+	allModals[0].close()
+}
+
+const showBrowserResultsFunction = () => {
+	allModals[1].showModal()
+	hideSpinnerFunction()
 }
 
 allNavItems[3].addEventListener('click', showLoginPageFunction)
 closeModalIcon.addEventListener('click', closeLoginPageFunction)
+searchButton.addEventListener('click', showBrowserResultsFunction)
+searchInput.addEventListener('keypress', e => {
+	if (e.key === 'Enter') {
+		showBrowserResultsFunction()
+	}
+})
